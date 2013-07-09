6d98a03cbe7761672b0668302fa269ca9ebfef3a
Timon de Groot <tdegroot96@gmail.com>
2013-07-09 23:45:51 +0200

    Enlarged height of screen

--- src/nl/tdegroot/games/nemesis/RunGame.java
+++ src/nl/tdegroot/games/nemesis/RunGame.java
@@ -8,7 +8,7 @@ public class RunGame {
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer game = new AppGameContainer(new Nemesis());
 
-		game.setDisplayMode(1280, 720, false);
+		game.setDisplayMode(1280, 768, false);
 		game.setShowFPS(true);
 		game.setMaximumLogicUpdateInterval(17);
 		game.setMinimumLogicUpdateInterval(17);
